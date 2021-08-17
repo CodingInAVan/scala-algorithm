27d7866c1f4c2d7c9a8e77bb3b41b3c163ced3cd
Myoungho Shin <mjjmmjj@gmail.com>
2021-08-17 15:18:25 -0700

    Using foldleft

--- src/main/scala/leetcode/LetterCombinationsPhoneNumber.scala
+++ src/main/scala/leetcode/LetterCombinationsPhoneNumber.scala
@@ -2,14 +2,14 @@ package leetcode
 
 object LetterCombinationsPhoneNumber extends App {
   def numPadAndChars = Map(
-    '2' -> List('a','b','c'),
-    '3' -> List('d','e','f'),
-    '4' -> List('g','h','i'),
-    '5' -> List('j','k','l'),
-    '6' -> List('m','n','o'),
-    '7' -> List('p','q','r','s'),
-    '8' -> List('t','u','v'),
-    '9' -> List('w','x','y','z')
+    '2' -> List("a","b","c"),
+    '3' -> List("d","e","f"),
+    '4' -> List("g","h","i"),
+    '5' -> List("j","k","l"),
+    '6' -> List("m","n","o"),
+    '7' -> List("p","q","r","s"),
+    '8' -> List("t","u","v"),
+    '9' -> List("w","x","y","z")
   )
 
   def letterCombinations(digits: String): List[String] = {
@@ -17,18 +17,13 @@ object LetterCombinationsPhoneNumber extends App {
     val input = for {
       c <- digits.toCharArray
     } yield numPadAndChars(c)
-
-    def iterate(list: List[Char], idx: Int, solution: String, solutions: List[String]): List[String] = {
-      if(idx == input.length || list.isEmpty) return solution :: solutions
-
-      val lst = if(idx >= input.length - 1) List() else input(idx + 1)
-      (for {
-        c <- list
-      } yield iterate(lst, idx + 1, solution + c, solutions)).flatten
-    }
-
-    iterate(input(0), 0, "", List())
+    input.tail.foldLeft[List[String]](input.head)((a, b) ⇒
+      a ++ (for {
+        i ← a
+        j ← b
+      } yield i + j)
+    ).filter(s ⇒ s.size == digits.length)
   }
 
-  println(letterCombinations("234"))
+  println(letterCombinations("2345"))
 }
